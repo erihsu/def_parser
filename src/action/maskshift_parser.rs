e1aac5bf8212d78708d622fc6416bc6ef837d253
erihsu <zhenyuxu163@163.com>
2020-11-05 21:50:13 +0800

    update

--- src/action/maskshift_parser.rs
+++
@@ -1,9 +0,0 @@
-use crate::action::common_parse::{tstring, ws};
-use nom::bytes::complete::tag;
-use nom::multi::many1;
-use nom::sequence::delimited;
-use nom::IResult;
-
-pub fn maskshift_section(input: &str) -> IResult<&str, Vec<&str>> {
-    delimited(tag("COMPONENTMASKSHIFT"), many1(tstring), ws(tag(";")))(input)
-}
